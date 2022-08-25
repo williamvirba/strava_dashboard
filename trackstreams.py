731b0faeb839275bd3f8fe24a0d5c4c1cced4726
williamvirba <86878465+williamvirba@users.noreply.github.com>
2022-08-25 12:38:22 +0200

    Update trackstreams.py

--- trackstreams.py
+++ trackstreams.py
@@ -51,11 +51,11 @@ df['cum_distance'] = df['distance'].cumsum()
 df['Date'] = df['time'].str[:10]
 df['Time'] = df['time'].str[11:19]
 df['Time'] = pd.to_datetime(df['Time'],format= '%H:%M:%S').dt.time
-df['Times'] = pd.to_datetime(df['Time'],format= '%H:%M:%S').dt.second
-df['Timem'] = pd.to_datetime(df['Time'],format= '%H:%M:%S').dt.minute
-df['Timeh'] = pd.to_datetime(df['Time'],format= '%H:%M:%S').dt.hour
-df['Timess'] = df['Times']+df['Timem']*60+df['Timem']*3600
-df['Timed'] = df['Timess'].diff()
+#df['Times'] = pd.to_datetime(df['Time'],format= '%H:%M:%S').dt.second
+#df['Timem'] = pd.to_datetime(df['Time'],format= '%H:%M:%S').dt.minute
+#df['Timeh'] = pd.to_datetime(df['Time'],format= '%H:%M:%S').dt.hour
+#df['Timess'] = df['Times']+df['Timem']*60+df['Timem']*3600
+#df['Timed'] = df['Timess'].diff()
 
 df['Speed'] = df['distance']/df['Timed']*3600/1000
 

b07279842bcc68bf277d765b7ba90ff5d9a03706
williamvirba <86878465+williamvirba@users.noreply.github.com>
2022-10-10 18:35:23 +0200

    Update trackstreams.py

--- trackstreams.py
+++ trackstreams.py
@@ -171,7 +171,7 @@ normd = mpl.colors.BoundaryNorm(boundaries=myfeelbounds, ncolors=len(cdmap.color
 
 
 
-plot=ax.scatter(x,y,c=c,cmap=cdmap,s=df["Speed"]/10,norm=normd)
+plot=ax.scatter(x,y,c=c,cmap=cdmap,s=2,norm=normd)
 plt.colorbar(plot,spacing='proportional',label="W Heart Rate scale",ticks=myfeelbounds)
 plt.title('MAP', loc='center')
 st.write(figas)
